1a2933a17ebc1291b9ccc489dae34a8a0e9eb176
gurrier <tmugan@gmail.com>
2025-08-21 12:20:15 +1000

    Update config_flow.py
    Fixed a deprecation before HASS 2025.12

--- custom_components/localvolts/config_flow.py
+++ custom_components/localvolts/config_flow.py
@@ -66,7 +66,7 @@ class LocalvoltsOptionsFlowHandler(config_entries.OptionsFlow):
 
     def __init__(self, config_entry):
         """Initialize Localvolts options flow."""
-        self.config_entry = config_entry
+        super().__init__(config_entry)
 
     async def async_step_init(self, user_input=None) -> FlowResult:
         """Manage the options."""
